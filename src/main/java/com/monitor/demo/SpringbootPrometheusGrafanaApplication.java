910c29ea7919ad775ce251480b399d8e0a3ff998
Fang Ming <fangming1987200@gmail.com>
2022-03-26 21:24:46 +0800

    Initialize

---
+++ src/main/java/com/monitor/demo/SpringbootPrometheusGrafanaApplication.java
@@ -0,0 +1,13 @@
+package com.monitor.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPrometheusGrafanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPrometheusGrafanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/monitor/demo/SpringbootPrometheusGrafanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monitor.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPrometheusGrafanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
